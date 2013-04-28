c2284605fec517f7fd97a2f87b5874709606a23f
KnowYourAPI <addictedtoicetea@gmail.com>
2013-04-28 02:17:41 +0200

    Added first simple implementation of compliment-building

--- src/bot/FileReader.scala
+++ src/bot/FileReader.scala
@@ -5,17 +5,27 @@ import scala.util.Random
 
 class FileReader {
   
-  def getRandomLine(fileName : String) : String = {
-    
-	  val bufferedSource = Source.fromFile(fileName)
+  def getRandomLine(filePath : String) : String = {
+	  val bufferedSource = Source.fromFile(filePath)
 	  val (it1, it2) = bufferedSource.getLines.duplicate
 	  val numOfLines = it1.size
 	  val random = new Random()
 	  val lineIndex = random.nextInt(numOfLines) + 1
 	  it2.drop(lineIndex-1)
 	  
-	  return it2.next()
-	  
+	  return it2.next() 
+  }
+  
+  def gatherUniqueLines(filePath : String, numberOfLines : Int) : List[String] = {
+    var lines = List[String]()
+    
+    while(lines.size < numberOfLines) {
+      val randomLine = getRandomLine(filePath)
+      if(!lines.contains(randomLine))
+    	  lines = randomLine :: lines
+    }
+    
+    return lines
   }
 
 }
--- src/bot/HarmonyBot.scala
+++ src/bot/HarmonyBot.scala
@@ -7,15 +7,43 @@ import twitter4j.conf.ConfigurationBuilder
 import twitter4j.TwitterStreamFactory
 import twitter4j.UserStreamListener
 
-object BotStarter {
+object HarmonyBot {
+  
+  val ADJECTIVES_FILE = "res/adjectives.txt"
+  val SENTENCE_FILE = "res/sentenceTemplates.txt"
+  val PERSON_FILE = "res/personDescriptors.txt"
+    
+  val ADJECTIVE_PLACEHOLDER = "<adjective>"
+  val NOUN_PLACEHOLDER = "<noun>"
   
   def main(args: Array[String]) {
     
-    val harmonyListener = new HarmonyUserListener()
+//    val harmonyListener = new HarmonyUserListener()
+//    
+//    val twitterStream = TwitterStreamFactory.getSingleton();
+//    twitterStream.addListener(harmonyListener)
+//    twitterStream.user();
+	  val fileReader = new FileReader()
+	  
+	  val sentenceTemplate = fileReader.getRandomLine(SENTENCE_FILE)
+	  val adjectivePlaceholders = countPlaceholders(sentenceTemplate, ADJECTIVE_PLACEHOLDER)
+	  val nounPlaceholders = countPlaceholders(sentenceTemplate, NOUN_PLACEHOLDER)
+	  val adjectiveList = fileReader.gatherUniqueLines(ADJECTIVES_FILE, adjectivePlaceholders)
+	  val nounList = fileReader.gatherUniqueLines(PERSON_FILE, nounPlaceholders)
+	  
+	  val compliment = buildCompliment(sentenceTemplate, adjectiveList, nounList)
+	  println(compliment)
+  }
+  
+  def countPlaceholders(sentenceTemplate : String, placeholder : String) : Int = {
+    return sentenceTemplate.split(" ").count((s : String) => s.contains(placeholder))
+  }
+  
+  def buildCompliment(sentenceTemplate : String, adjectives : List[String], nouns : List[String]) : String = {
+    val complimentWithAdjectives = adjectives.foldLeft(sentenceTemplate)((b,a) => b.replaceFirst(ADJECTIVE_PLACEHOLDER, a))
+    val fullCompliment = nouns.foldLeft(complimentWithAdjectives)((b,a) => b.replaceFirst(NOUN_PLACEHOLDER, a))
     
-    val twitterStream = TwitterStreamFactory.getSingleton();
-    twitterStream.addListener(harmonyListener)
-    twitterStream.user();
+    return fullCompliment
   }
 
 }
